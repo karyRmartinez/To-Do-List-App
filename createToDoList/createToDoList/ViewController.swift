6fff28631f5c5bfc1d1c686e19879db5e58b13b1
karyRmartinez <karymartinez@pursuit.org>
2020-04-24 15:13:51 -0400

    setting up SceneDelegate

---
+++ createToDoList/createToDoList/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  createToDoList
+//
+//  Created by Kary Martinez on 4/24/20.
+//  Copyright © 2020 Kary Martinez. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
