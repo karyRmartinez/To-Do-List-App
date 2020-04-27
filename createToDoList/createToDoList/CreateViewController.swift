130a0eb27cf88e756b421e11dd0518cc9890bb6c
karyRmartinez <karymartinez@pursuit.org>
2020-04-26 20:10:15 -0400

    Constraints for textField and subviews added

--- createToDoList/createToDoList/CreateViewController.swift
+++ createToDoList/createToDoList/CreateViewController.swift
@@ -17,22 +17,30 @@ class CreateViewController: UIViewController {
             ToDoField.backgroundColor = .white
         return ToDoField
     }()
+    
+    
+    private func setTextFieldConstraints() {
+        NSLayoutConstraint.activate([
+            ListOfToDosTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            ListOfToDosTextField.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -200),
+            ListOfToDosTextField.widthAnchor.constraint(equalToConstant: 300),
+            ListOfToDosTextField.heightAnchor.constraint(equalToConstant: 35)
+        ])
+    }
+    
+    private func addSubviews() {
+        self.view.addSubview(ListOfToDosTextField)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        addSubviews()
+        setTextFieldConstraints()
 
         // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
